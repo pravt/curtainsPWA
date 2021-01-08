5b86a53807e54fe453882e12b5812795942e3518
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-01-08 14:05:58 +0000

    pwa icon change

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `src/static/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/static/curtain.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
