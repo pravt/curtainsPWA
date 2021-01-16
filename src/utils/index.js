9edf91e1d2b15038541765236c5f7403bd16d986
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-01-16 14:11:31 +0000

    adding checks on pdf

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,15 @@
-export const getPDfDocuments = (data) => {
-    const documents = data.prismicBlogpost.data.body.filter(item=>item['__typename'] === 'PrismicBlogpostBodyPdfslice').map(i=>i.primary)[0];
-   var result = [];
-  
-   documents &&  Object.keys(documents).map(k=>{
-        if(documents[k].name.indexOf('.pdf')>0){
-            result.push(documents[k]);
-        }
+const bodyName = 'PrismicBlogpostBodyPdfslice'
+export const getPDfDocuments = data => {
+  let documents = data.prismicBlogpost.data.body
+    .filter(item => item['__typename'] === bodyName)
+    .map(i => i.primary)[0]
+  var result = []
+
+  documents &&
+    Object.keys(documents).map(k => {
+      if (documents[k].name && documents[k].name.indexOf('.pdf') > 0) {
+        result.push(documents[k])
+      }
     })
-    return result;
+  return result
 }
