86b9752280f9c0a755037429f66edbb03fc9b8de
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-01-16 14:17:16 +0000

    increase widht abd height

--- src/components/pdfViewer/index.js
+++ src/components/pdfViewer/index.js
@@ -11,7 +11,7 @@ const urlTorender = "https://drive.google.com/viewerng/viewer?embedded=true&url=
         Close
       </button>
         <div className="pdfIframeContainer">
-        <embed src={urlTorender} width="500" height="375"/>
+        <embed src={urlTorender} width="100%" height="100%"/>
         </div>
         </>
   )
