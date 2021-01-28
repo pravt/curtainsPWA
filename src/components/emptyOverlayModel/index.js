20f7c8175c26604b54eb14a1f5d00be501f019a4
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-01-28 13:12:05 +0000

    right icons sets

--- src/components/emptyOverlayModel/index.js
+++ src/components/emptyOverlayModel/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import _1 from './1.png'
+import _2 from './2.png'
+import _3 from './3.png'
 function EmptyOverlayModel({
     removeOverlay
 }) {
@@ -10,6 +12,11 @@ function EmptyOverlayModel({
         className="overlay-close"
         onClick={e => removeOverlay()}
       />
+      <div className="rightContainerFlex">
+      <img className="box img-fluid" src={_1}></img>
+      <img className="box img-fluid"  src={_2}></img>
+      <img className="box img-fluid"  src={_3}></img>
+      </div>
     </div>
   )
 }
