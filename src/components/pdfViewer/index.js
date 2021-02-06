2a00ef40553a2d0a4ab520289d7cb0313608adee
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-02-06 17:20:16 +0000

    added google doc url on mobile view

--- src/components/pdfViewer/index.js
+++ src/components/pdfViewer/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { isMobile } from 'react-device-detect';
 function PdfViewer({ fileURL, closePreview }) {
-    //var encodedUrl = encodeURIComponent(fileURL);
-    //let googleDocsBaseURL = 'https://docs.google.com/viewer?url=';
-
-    //console.log(" file url ",fileURL);
-
-    //const pdfURL = googleDocsBaseURL +encodedUrl + '&embedded=true';
-    //const u ="https://360host.me/brochure/Portfolio/v1.pdf"
+    
     if (isMobile) {
+        let encodedUrl = encodeURIComponent(fileURL);
+        let googleDocsBaseURL = 'https://docs.google.com/viewer?url=';
+        const pdfURL = googleDocsBaseURL +encodedUrl + '&embedded=true';
         return (
             <div>
-                <object name="frame_1" data={fileURL} type="text/pdf" max-width="100%" height="800px">
-                    <embed src={fileURL} id="embed_pdf" />
+                <object name="frame_1" data={pdfURL} type="text/pdf" max-width="100%" height="800px">
+                    <embed src={pdfURL} id="embed_pdf" />
                 </object>
             </div>
         );
