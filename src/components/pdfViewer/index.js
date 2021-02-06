b01d043707700f22a7d82d27cb7cd08fd1b62714
sekhar babu karri <Sekhar-Babu.Karri@lloydsbanking.com>
2021-02-06 17:05:07 +0000

    added pdf to render in mobile and desktop

--- src/components/pdfViewer/index.js
+++ src/components/pdfViewer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 function PdfViewer({ fileURL, closePreview }) {
     //var encodedUrl = encodeURIComponent(fileURL);
     //let googleDocsBaseURL = 'https://docs.google.com/viewer?url=';
@@ -9,19 +9,27 @@ function PdfViewer({ fileURL, closePreview }) {
     //const pdfURL = googleDocsBaseURL +encodedUrl + '&embedded=true';
     //const u ="https://360host.me/brochure/Portfolio/v1.pdf"
     if (isMobile) {
-        return (<div> This content is unavailable on mobile</div>);
-    }
-    return (
-        <>
-            <button type="button" className="overlay-close pdf-viewer-close" onClick={e => closePreview()}>
-                Close
-            </button>
-
-            <div className="scroll-wrapper">
-                <iframe title="image" src={fileURL} frameBorder="0" allowFullScreen />
+        return (
+            <div>
+                <object name="frame_1" data={fileURL} type="text/pdf" max-width="100%" height="800px">
+                    <embed src={fileURL} id="embed_pdf" />
+                </object>
             </div>
-        </>
-    );
+        );
+    }else{
+        return (
+            <>
+                <button type="button" className="overlay-close pdf-viewer-close" onClick={e => closePreview()}>
+                    Close
+                </button>
+    
+                <div className="scroll-wrapper">
+                    <iframe title="image" src={fileURL} frameBorder="0" allowFullScreen />
+                </div>
+            </>
+        );      
+    }
+  
 }
 
 PdfViewer.defaultProps = {};
